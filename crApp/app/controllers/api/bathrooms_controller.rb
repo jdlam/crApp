493c924e32b369cc022e5b8a6d4f6b5e0e17d3b1
Jonathan Lam <jdlam420@gmail.com>
2015-09-16 11:17:17 -0400

    retrieves geolocation data successfully, and searches for nearby restrooms

--- crApp/app/controllers/api/bathrooms_controller.rb
+++ crApp/app/controllers/api/bathrooms_controller.rb
@@ -8,4 +8,23 @@ class Api::BathroomsController < ApplicationController
     render json: Bathroom.find(params[:id])
   end
 
+  def locate
+    coordinates = search_params
+    radius = params[:radius].to_f
+    distance = radius/70
+    latMin = coordinates[:latitude].to_f - distance
+    latMax = coordinates[:latitude].to_f + distance
+    longMin = coordinates[:longitude].to_f - distance
+    longMax = coordinates[:longitude].to_f + distance
+    results = Bathroom.where({latitude: (latMin..latMax), longitude: (longMin..longMax)})
+
+    render json: results
+  end
+
+  private
+
+  def search_params
+    params.require(:coords).permit(:latitude, :longitude)
+  end
+
 end
