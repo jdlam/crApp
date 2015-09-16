cca641a23159f19421e2ab3eb041dccb7b6c664c
VIDIT SAXENA <ViditSaxena@VIDITs-MacBook-Pro.local>
2015-09-16 18:42:45 -0400

    adds snazziness to maps

--- crApp/app/assets/javascripts/map.js
+++ crApp/app/assets/javascripts/map.js
@@ -144,11 +144,17 @@ $(function() {
 
 	};
 
-	//Adding infowindow option
-	// infowindow = new google.maps.InfoWindow({
-	// 	content: "holding..."
-	// });
+	var styles = [{"featureType":"administrative","elementType":"labels.text.fill","stylers":
+	[{"color":"#444444"}]},{"featureType":"landscape","elementType":"all","stylers":[{"color":"#f2f2f2"}]},{"featureType":"poi","elementType":"all","stylers":[{"visibility":"off"}]},
+	{"featureType":"road","elementType":"all","stylers":[{"saturation":-100},{"lightness":45}]},
+	{"featureType":"road.highway","elementType":"all","stylers":[{"visibility":"simplified"}]},
+	{"featureType":"road.arterial","elementType":"labels.icon","stylers":[{"visibility":"off"}]},
+	{"featureType":"transit","elementType":"all","stylers":[{"visibility":"off"}]},
+	{"featureType":"water","elementType":"all","stylers":[{"color":"#46bcec"},{"visibility":"on"}]}]
+
+
 
 	//Fire up Google maps and place inside the map-canvas div
 	map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+	map.setOptions({styles: styles});
 });
